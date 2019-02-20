f65a2d98d67393a1147c5e51c30cb551eb47be00
ARATA <newbluelight1031@gmail.com>
2019-02-20 23:57:33 +0000

    create posts by category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -60,6 +60,10 @@ class CategoriesController < ApplicationController
       format.json { head :no_content }
     end
   end
+  
+  def category_post
+     @category = Category.find(params[:id])
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
