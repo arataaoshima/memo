8685bd03da300d7d3ccf6e826ba600a23c332b42
ARATA <newbluelight1031@gmail.com>
2019-02-24 07:17:32 +0000

    clean style of login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
+devise_for :users, :controllers => {
+  :sessions      => "users/sessions"
+}
   resources :categories
   get 'categories/post/:id' => 'categories#category_post'
   # The priority is based upon order of creation: first created -> highest priority.
